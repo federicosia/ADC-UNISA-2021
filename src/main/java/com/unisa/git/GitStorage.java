738a2ed4630ddd1eb81f889e07c137f9706bcb1b
Federico Siano <federicosiano1@gmail.com>
2021-12-28 13:26:52 +0100

    fix import

--- src/main/java/com/unisa/git/GitStorage.java
+++ src/main/java/com/unisa/git/GitStorage.java
@@ -7,7 +7,6 @@ import net.tomp2p.dht.FutureGet;
 import net.tomp2p.dht.PeerBuilderDHT;
 import net.tomp2p.dht.PeerDHT;
 import net.tomp2p.futures.FutureBootstrap;
-import net.tomp2p.p2p.Peer;
 import net.tomp2p.p2p.PeerBuilder;
 import net.tomp2p.peers.Number160;
 import net.tomp2p.peers.PeerAddress;
